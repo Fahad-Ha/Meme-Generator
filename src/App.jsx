7fbfd4bf5d988bde05598a5369216a29d73c6313
Fahad <fahhod.ahmad@gmail.com>
2023-03-31 03:24:53 +0300

    initial version

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from '../components/Header'
+import Meme from '../components/Meme'
+
+function App() {
+
+  return (
+    <div>
+      <div className='header-background'>
+        <Header />
+        <Meme />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Input.jsx
@@ -0,0 +1,13 @@
+export default function Input() {
+    return (
+        <>
+        <div>
+            <input></input>
+            <input></input>
+            <div>
+                <button type="submit">Get a New meme image</button>
+            </div>
+            </div>
+        </>
+    )
+}
